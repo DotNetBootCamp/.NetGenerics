682cc6632b67ccd93bc6184210bcc961cc1ac744
Ryan Kotzen <Ryank@entelect.co.za>
2015-02-11 11:21:18 +0200

    Renaming generic parameter as per code guidelines.

--- Source/GenericList.cs
+++ Source/GenericList.cs
@@ -2,27 +2,27 @@
 
 namespace Source
 {
-    public class GenericList<T>
-        where T : class
+    public class GenericList<TListItem>
+        where TListItem : class
     {
-        private readonly List<T> _cheatingUnderlyingList;
+        private readonly List<TListItem> _cheatingUnderlyingList;
 
         public GenericList()
         {
-            _cheatingUnderlyingList= new List<T>();
+            _cheatingUnderlyingList= new List<TListItem>();
         }
 
-        public void Add(T input)
+        public void Add(TListItem input)
         {
             _cheatingUnderlyingList.Add(input);
         }
 
-        public T Get(int index)
+        public TListItem Get(int index)
         {
             return _cheatingUnderlyingList[index];
         }
 
-        public void InsertAt(int index, T input)
+        public void InsertAt(int index, TListItem input)
         {
             _cheatingUnderlyingList.Insert(index, input);
         }
